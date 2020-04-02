65b06b73cab1d4f2be14d0e29fd7ae692ab7e304
James Smith <james-smith-za@users.noreply.github.com>
2020-04-02 17:42:49 +0200

    Mostly documentation, some small logic changes.

--- Test.hpp
+++ Test.hpp
@@ -1,24 +1,40 @@
 #ifndef __TEST_HPP__
 #define __TEST_HPP__
 
-//TODO: Doxygen documentation for the class.
-// Also a short explanation of how-to-use, either in the README.md but probably in the doxygen \detail section.
+///\todo Examples of how to use this class.
+///\todo brief and detailed descriptions.
 class Test
 {
     public:
-        virtual void ~Test() {};
+        /// The destructor must be virtual to ensure that derived classes' destructors are also called when the objects are destroyed.
+        virtual ~Test();
 
+        /// Execute the test.
         void  run_test();
+        /// Retrieve the test result.
         int   get_result();
+        /// Retrieve the amount of time that the test took.
         float get_time();
     
     protected:
+        // The constructor is protected as a reminder that we can't instantiate a pure virtual class directly.
+        Test();
+
+        // These functions make the class pure virtual and must be implelented by derived classes.
+        ///\brief Generate simulated input data.
         virtual void simulate_input() = 0;
         virtual void run_operation()  = 0;
-        virtual void verify_output()  = 0;
+        virtual int  verify_output()  = 0;
     
     private:
+        /// For recording the start of the test.
+        cudaEvent_t m_StartEvent;
+        /// For recording the end of the test.
+        cudaEvent_t m_StopEvent;
+
+        /// Stores the elapsed time of the test once completed. Unit: ms.
         float m_fElapsedTime_ms;
+        /// Stores the result of the test.
         int   m_iResult;
 }
 
