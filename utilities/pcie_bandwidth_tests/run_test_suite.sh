3be4718ddfa5a452160ca5967a40104585ace683
Gareth Callanan <gcallanan@ska.ac.za>
2020-04-15 21:07:41 +0000

    Created a bash script that runs a full suite of bandwidth tests for every number of GPU devices, unidirectional and bidirectional PCIe transfers, and from 0 to 50 threads. This script generates a number of csv output files for plotting later

---
+++ utilities/pcie_bandwidth_tests/run_test_suite.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+numGpus=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+echo "Running Full Suite of Bandwidth Tests"
+echo "$numGpus Nvidia Device(s) detected"
+PCIeDirFlags="-s -d"
+SecondsPerTest=60
+MaxThreads=50
+
+timestamp=$(date +"%Y%m%d_%H%M")
+gpuMask_dec=0
+for i in `seq 0 $numGpus`
+do  
+    baseName="BWTests_${timestamp}_${i}GPUs"
+    echo Performing Test: $baseName
+    gpuMask_bin=$(echo "obase=2;$gpuMask_dec" | bc)
+    if [[ $i -ne 0 ]]; then 
+        ./test_bandwidth -g  $gpuMask_bin $PCIeDirFlags -t $SecondsPerTest -b -m 0 -M $MaxThreads -c | tee ${baseName}_pcie_bidir.txt
+        ./test_bandwidth -g  $gpuMask_bin -d -t $SecondsPerTest -b -m 0 -M $MaxThreads -c | tee ${baseName}_pcie_D2H.txt
+        ./test_bandwidth -g  $gpuMask_bin -s -t $SecondsPerTest -b -m 0 -M $MaxThreads -c | tee ${baseName}_pcie_H2D.txt
+    else
+        ./test_bandwidth -g  $gpuMask_bin $PCIeDirFlags -t $SecondsPerTest -b -m 0 -M $MaxThreads -c | tee ${baseName}.txt
+    fi
+    gpuMask_dec=$((($gpuMask_dec<<1)+1))
+done
