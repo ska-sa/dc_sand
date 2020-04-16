c2ff368dbe021afe7bee4d6a2d456cf671f2044e
Gareth Callanan <gcallanan@ska.ac.za>
2020-04-16 13:35:35 +0000

    Added a script to continuously transfer data across the PCIe busses and report this to the terminal

---
+++ utilities/pcie_bandwidth_tests/pcie_transfer_cts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Script for continously transferring data to the PCIe devices. Will output rate to terminal every 20 seconds
+
+echo "PCIe Continous Transfer Script"
+
+if [ "$#" -eq 0 ]; then
+    numGpus=1
+else
+    numGpus=$1
+fi
+echo "Performing transfers on $numGpus GPUs or maximum number of installed GPUs, whichever is lower"
+
+gpuMask=""
+for i in `seq 0 $numGpus`
+do  
+    gpuMask=${gpuMask}1
+done
+
+while :
+do
+	./test_bandwidth -g $gpuMask -d -s -t 20 -c
+done
