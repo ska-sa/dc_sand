ce0b34b6455a11a4e3fd3291be7ac1c3db52ddfb
James Smith <james-smith-za@users.noreply.github.com>
2020-05-11 09:12:34 +0000

    not an especially meaningful commit. Need to push.

--- precise_beamformer/precise_beamformer.py
+++ precise_beamformer/precise_beamformer.py
@@ -3,6 +3,8 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 from pycuda.compiler import SourceModule
 import numpy as np
+import time
+from typing import Union
 
 
 iTimeLenghTotal = 256
@@ -19,10 +21,13 @@ AD_pin = cuda.register_host_memory(AntennaData)
 AD_gpu = cuda.mem_alloc(AD_pin.nbytes)
 
 # Simulated delay-val data.
-DelayVals = np.random.randn(iNBeams, iNAntennas, 4) # Don't know yet whether or not the negative numbers generated in this way will hurt.
+DelayVals = np.random.randn(iNBeams, iNAntennas, 4) # 4 because {delay, delay_rate, phase, phase_rate}
+                                                    # Don't know yet whether or not the negative numbers generated in this way will hurt.
 DelayVals = DelayVals.astype(np.float32)
 DV_pin = cuda.register_host_memory(DelayVals)
 DV_gpu = cuda.mem_alloc(DV_pin.nbytes)
+
+#TODO simulate some time-related information. Will need to pass this to the kernel as well.
 
 # Transfer
 cuda.memcpy_htod(AD_gpu, AD_pin) #This is the one we'll want to time, the other is going to happen less frequently so it's not as important.
@@ -46,4 +51,33 @@ precise_beamformer = module.get_function("precise_beamformer")
 cuda.memcpy_dtoh(O_pin, O_gpu)
 
 # Verify results
-;
+def gen_steering_coeffs(referenceTime: Union[float, time.struct_time],
+                        currentTime: Union[float, time.struct_time],
+                        samplingPeriod: float,
+                        nChannels: int,
+                        nAntennas: int,
+                        nBeams: int,
+                        lDelayVals: np.ndarray) -> np.ndarray:
+    """
+    This function needs to return an array of shape [CxAxB] so that it fits with the matmul assumption.
+    """
+    steeringCoeffs = np.empty((nChannels, nAntennas, nBeams), dtype=np.complex64)
+    for a in range(nAntennas):
+        for b in range(nBeams):
+            delay, delay_rate, phase, phase_rate = lDelayVals[i, j]
+            deltaTime = currentTime - referenceTime
+            deltaDelay = delay_rate*deltaTime
+            delay_N_2 = (delay + deltaDelay)*np.pi/(samplingPeriod*2)
+            channels = np.arange(nChannels)
+            delay_n = (delay + deltaDelay)*channels/2*np.pi/(samplingPeriod*nChannels)
+            deltaPhase = phase_rate*deltaTime
+            phase_0 = phase - delay_N_2 + deltaPhase
+            rotation = delay_n + phase_0
+            cplx_beamweights = np.cos(rotation) + 1j*sin(rotation)
+            cplx_beamweights = cplx_beamweights.astype(np.complex64)
+            steeringCoeffs[:,a,b] = cplx_beamweights
+    return steeringCoeffs
+
+
+foo = gen_steering_coeffs(ref_time,)
+
