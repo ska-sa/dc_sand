68b6231130b455763880a51466939208624fc4d4
James Smith <james-smith-za@users.noreply.github.com>
2020-05-14 13:24:52 +0200

    Last commit. This has been mostly abandoned as it doesn't seem to be needed.

--- precise_beamformer/precise_beamformer.cu
+++ precise_beamformer/precise_beamformer.cu
@@ -2,5 +2,10 @@ __global__ void precise_beamformer(float4 DelayVals[N_BEAMS][N_ANTENNAS],
                                    char2 AntennaData[N_ANTENNAS][N_CHANS][TOTAL_TIME],
                                    cuComplex Output[TOTAL_TIME][N_CHANS][N_BEAMS] )
 {
-    
+    //Shared memory space to store antenna-data (AD)
+    __shared__ char2 sAntennaData[TIME_STEP][N_ANTENNAS][N_CHANS]; //__attribute((aligned(32))); //not sure what this actually does?
+    //Retrieve AD from global memory.
+    sAntennaData
 }
+
+
