8c7fbbbd5f898b296447ce1913f948e3a93fd2de
James Smith <james-smith-za@users.noreply.github.com>
2020-05-19 15:39:34 +0200

    Correct the way we do pinned host memory in tc_dynamic_range.py

--- tc_dynamic_range/tc_dynamic_range.py
+++ tc_dynamic_range/tc_dynamic_range.py
@@ -9,17 +9,17 @@ tcm_size = 16
 def print_mat(matrix):
     """A quickly-and-dirty function to show me an entire 16x16 matrix."""
     title_string = f"{'': >3}"
-    for x in range(3):
+    for x in range(tcm_size):
         title_string += f"{x: >12}"
     print(title_string)
-    for i in range(3):
+    for i in range(tcm_size):
         output_string = ""
-        for j in range(3):
+        for j in range(tcm_size):
             output_string += f"{matrix[i,j]:10.5f}"
             output_string += "  "
         print(f"{i: >3}: {output_string}")
 
-# This is the py-cuda way of getting the kernel from a source file.
+# This is the pycuda way of getting the kernel from a source file.
 # Technically it wants a raw string, but I like to keep files separate. So I use this method to read
 # a file into a string directly into the source module's __init__ function.
 module = SourceModule(open("tc_dynamic_range.cu").read(),
@@ -31,45 +31,34 @@ module = SourceModule(open("tc_dynamic_range.cu").read(),
 simple_tc_matmul_kernel = module.get_function("simple_tc_matmul")
 
 # Set up the A matrix. Currently, it is mostly zeros.
-a = np.zeros((tcm_size, tcm_size), dtype=np.float16)
-a[0,0] = 512
-a[0,2] = 0.125
-# Columns 3 and 8 will be ones to match up with equivalent rows in B. 
-#a[:,3] = 1.0
-#a[:,8] = 1.0
-a_pin = cuda.register_host_memory(a)
-a_gpu = cuda.mem_alloc(a_pin.nbytes)
-cuda.memcpy_htod(a_gpu, a_pin)
+a_host = cuda.pagelocked_zeros((tcm_size, tcm_size), dtype=np.float16)
+a_host[0,0] = 10
+a_host[0,2] = 0.125
+a_device = cuda.mem_alloc(a_host.nbytes)
+cuda.memcpy_htod(a_device, a_host)
 
 print("A:")
-print_mat(a)
+print_mat(a_host)
 
 # B will be mostly zeros.
-b = np.zeros((tcm_size, tcm_size), dtype=np.float16)
-b[0,0] = 512
-b[2,0] = 0.125
-# Row 3 will be a small number
-#b[3,:] = 0.0625 #i.e. 1/16
-#Row 8 starts at 2 and doubles until it reaches 65535 (or tries to)
-#for i in range(tcm_size):
-#    b[8,i] = 2*np.power(2,i)
-b_pin = cuda.register_host_memory(b)
-b_gpu = cuda.mem_alloc(a_pin.nbytes)
-cuda.memcpy_htod(b_gpu, b_pin)
+b_host = cuda.pagelocked_zeros((tcm_size, tcm_size), dtype=np.float16)
+b_host[0,0] = 1
+b_host[2,0] = 0.125
+b_device = cuda.mem_alloc(a_host.nbytes)
+cuda.memcpy_htod(b_device, b_host)
 
 print("B:")
-print_mat(b)
+print_mat(b_host)
 
-c = np.empty((tcm_size, tcm_size), dtype=np.float32)
-c_pin = cuda.register_host_memory(c)
-c_gpu = cuda.mem_alloc(c_pin.nbytes)
+c_host = cuda.pagelocked_zeros((tcm_size, tcm_size), dtype=np.float32)
+c_device = cuda.mem_alloc(c_host.nbytes)
 
 # We launch the kernel with 32 threads because to do tensor-core multiplication we need an entire warp.
-simple_tc_matmul_kernel(a_gpu, b_gpu, c_gpu, block=(32,1,1), grid=(1,1,1))
+simple_tc_matmul_kernel(a_device, b_device, c_device, block=(32,1,1), grid=(1,1,1))
 
-cuda.memcpy_dtoh(c_pin, c_gpu)
-context.synchronize()
+# Copy everything back once we're done.
+cuda.memcpy_dtoh(c_host, c_device)
 
 print("C:")
-print_mat(c)
+print_mat(c_host)
 
