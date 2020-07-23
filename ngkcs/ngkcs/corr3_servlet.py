bab72a33d301ae57aafe3dccc47b53eaafcb58da
James Smith <james-smith-za@users.noreply.github.com>
2020-07-23 15:22:48 +0200

    Added aiokatcp hint in README.
    The aiokatcp DeviceServer request_* functions are not that obvious if you haven't seen
    the DeviceServer examples, so I have put a link to them in the README.md.

--- ngkcs/ngkcs/corr3_servlet.py
+++ ngkcs/ngkcs/corr3_servlet.py
@@ -1,39 +1,48 @@
+"""
+Module needs a docstring.
+
+We should decide on a docstring convention.
+"""
+
 import aiokatcp
 
+
 class Corr3Servlet(aiokatcp.DeviceServer):
+    """Proof-of-concept DeviceServlet to demonstrate how Corr3 could work."""
+
     VERSION = "corr3_servlet-0.1"
     BUILD_STATE = "corr3_servlet-0.1.0"  # What is the convention with these?
-    
-    def __init__(self, *,  # I'm forcing all these arguments to be named, I find it helps with readability.
-                 name: str,
-                 n_antennas: int,
-                 n_channels: int,
-                 host: str,
-                 port: int,
-                 **kwargs):
+
+    def __init__(
+        self,
+        *,  # I'm forcing all these arguments to be named, I find it helps with readability.
+        name: str,
+        n_antennas: int,
+        n_channels: int,
+        host: str,
+        port: int,
+        **kwargs,
+    ):
+        """Init function also needs a docstring."""
         self.name = name
         self.n_antennas = n_antennas
         self.n_channels = n_channels
         super(Corr3Servlet, self).__init__(host=host, port=port, **kwargs)
 
-    async def request_beam_weights(self, ctx, data_stream: str, *weights):
+    async def request_beam_weights(self, ctx, data_stream, *weights):
         """Load weights for all inputs on a specified beam data-stream."""
-        if len(weights) != self.n_antennas: 
-            raise aiokatcp.connection.FailReply(f"{len(weights)} weights received, expected {self.n_antennas}") 
-        
+        if len(weights) != self.n_antennas:
+            raise aiokatcp.connection.FailReply(f"{len(weights)} weights received, expected {self.n_antennas}")
+
         if data_stream != "tied-array-channelised-voltage":
             return  # I wasn't quite clear whether this should be a problem. The way I read the ICD, no error should
-                    # be returned, the request should just do nothing.
+            # be returned, the request should just do nothing.
 
         for n, weight in enumerate(weights):
-            #TODO: Pass the message along to all the individual nodes. Currently just sending an inform.
+            # TODO: Pass the message along to all the individual nodes. Currently just sending an inform.
             ctx.inform(f"Antenna no {n} is being weighted by a factor of {weight.decode()}")
-            
-        
-        #TODO: The ICD says "the explanation describes the current weights applied to the inputs of a specific beam".
+
+        # TODO: The ICD says "the explanation describes the current weights applied to the inputs of a specific beam".
         #      I should probably figure out what that looks like, and return appropriately.
         #      The test should probably also assert this.
         return "Beam weights set correctly."
-        
-        
-
