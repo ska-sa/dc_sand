497b4c946450eb4540f1b11f2500feceb81ac4fe
James Smith <james-smith-za@users.noreply.github.com>
2020-07-23 15:26:11 +0200

    Renamed corr3_servlet_test_fixture to make it more explicit for Gareth.

--- ngkcs/testing/test_corr3_servlet.py
+++ ngkcs/testing/test_corr3_servlet.py
@@ -6,8 +6,9 @@ import aiokatcp
 
 CORR3_SERVLET_PORT = 7404
 
+
 @pytest.fixture(scope="function")
-async def corr3_servlet():
+async def corr3_servlet_test_fixture():
     """Provide an instance of Corr3Servlet to test.
 
     This fixture readies a Corr3Servlet instance in a running state, that can be queried and the results
@@ -21,11 +22,7 @@ async def corr3_servlet():
     is not a good idea. The main obstacle at the moment is that this involves changing the scope of the default
     `event_loop` fixture as well. I'm not sure how to do that yet either.
     """
-    servlet = Corr3Servlet(name="bob",
-                           n_antennas=4,
-                           n_channels=4096,
-                           host="0.0.0.0",
-                           port=CORR3_SERVLET_PORT)
+    servlet = Corr3Servlet(name="bob", n_antennas=4, n_channels=4096, host="0.0.0.0", port=CORR3_SERVLET_PORT)
     await servlet.start()
     # Using `yield` in a fixture allows execution to come back to the fixture once the test is done with it, so that
     # necessary cleanup can happen afterwards.
@@ -36,7 +33,7 @@ async def corr3_servlet():
 
 async def make_a_request(*args):
     """Connect to the servlet and send a request."""
-    client = await aiokatcp.Client.connect('localhost', CORR3_SERVLET_PORT)
+    client = await aiokatcp.Client.connect("localhost", CORR3_SERVLET_PORT)
     try:
         # We put this inside a `try/finally` block in order to make sure that everything gets cleaned up properly.
         # In some tests, we may expect an exception from deliberately sending a bad request, so we want to make sure
@@ -45,31 +42,30 @@ async def make_a_request(*args):
     finally:
         client.close()
         await client.wait_closed()
-    #TODO: In principle, we could `return` the reply, or parse it or something. I'm not quite sure what we need.
+    # TODO: In principle, we could `return` the reply, or parse it or something. I'm not quite sure what we need.
 
 
 class TestCorr3Servlet:
     """Class for grouping Corr3Servlet tests."""
+
     def test_beam_weights(self, corr3_servlet, event_loop):
         """Test a properly-formed ?beam-weights request."""
-        reply = event_loop.run_until_complete(make_a_request("beam-weights",
-                                                             "tied-array-channelised-voltage",
-                                                             "1", "2", "3", "4"))
-        #TODO: test whether the servlet passes the info on to the individual engines.
+        reply = event_loop.run_until_complete(
+            make_a_request("beam-weights", "tied-array-channelised-voltage", "1", "2", "3", "4")
+        )
+        # TODO: test whether the servlet passes the info on to the individual engines.
         #      This will require more infrastructure from the test fixture I think. Or another fixture.
-
 
     def test_beam_weights_wrong_numbers(self, corr3_servlet, event_loop):
         """Test a ?beam-weights request with the wrong number of weights being passed."""
         # This context manager tells `pytest` that we expect an exception to be raised.
         with pytest.raises(aiokatcp.connection.FailReply):
-            reply = event_loop.run_until_complete(make_a_request("beam-weights",
-                                                                 "tied-array-channelised-voltage",
-                                                                 "1", "2", "3")) # Only 3 weights, 4 expected.
-
+            reply = event_loop.run_until_complete(
+                make_a_request("beam-weights", "tied-array-channelised-voltage", "1", "2", "3")
+            )  # Only 3 weights, 4 expected.
 
     def test_beam_weights_wrong_stream(self, corr3_servlet, event_loop):
         """Test a ?beam-weights request with the wrong data-stream being passed."""
-        reply = event_loop.run_until_complete(make_a_request("beam-weights",
-                                                             "baseline-correlation-products", # i.e. not t-a-c-v.
-                                                             "1", "2", "3", "4"))
+        reply = event_loop.run_until_complete(
+            make_a_request("beam-weights", "baseline-correlation-products", "1", "2", "3", "4")  # i.e. not t-a-c-v.
+        )
