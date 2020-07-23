016fbfd44b55ee3230cec446ff7b73e437343eee
James Smith <james-smith-za@users.noreply.github.com>
2020-07-23 08:28:40 +0200

    Fix typo in setup.py.

--- ngkcs/setup.py
+++ ngkcs/setup.py
@@ -1,8 +1,13 @@
-from setuptools import setup, find_packages
+"""
+Installer for NGKCS module.
+
+This is very much proof-of-concept code.
+"""
+from setuptools import setup  # , find_packages
 
 setup(
-    name="ngcks",
+    name="ngkcs",
     version="0.1",
-    packages=["ngkcs",],
-    install_requres=["aiokatcp==0.8.0",], # Note, that this version will give deprecation warnings in Python 3.8.
+    packages=["ngkcs"],
+    install_requres=["aiokatcp==0.8.0"],  # Note, that this version will give deprecation warnings in Python 3.8.
 )
