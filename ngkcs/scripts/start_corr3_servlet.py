3b03996645ddd7d6ceda19d5ffef592ba8f8c842
James Smith <james-smith-za@users.noreply.github.com>
2020-07-28 10:33:30 +0200

    Revised code to ensure that the servlet works in manual testing.
    For this purposes, I created a `fake_node.py` script in the `debug` folder.
    
    Also made sure that everything passed all of Tyrone's checks, so that I could sleep peacefully at
    night knowing that my code complies with the standards.
    
    Still outstanding is the sensor aspect, and unit testing. Still working on that.

--- ngkcs/scripts/start_corr3_servlet.py
+++ ngkcs/scripts/start_corr3_servlet.py
@@ -1,21 +1,25 @@
 """
 Example of how to start a corr3 servlet.
+
 This is very rough-and-ready, proof-of-concept kind of code. Don't deploy into production.
 Dragons will appear from the void and breathe fire all over your baby seals.
 """
 import asyncio
 from ngkcs.corr3_servlet import Corr3Servlet
 
+
 async def main():
-    servlet = Corr3Servlet(name="stephen", # for historical reasons
-                           n_antennas=4,
-                           n_channels=4096,
-                           host="0.0.0.0",
-                           port=7404)
+    """Execute the program asynchronously if you would be so kind, good sir."""
+    servlet = Corr3Servlet(
+        name="stephen",  # for historical reasons
+        n_antennas=2,
+        x_engine_endpoints=[("localhost", 5678), ("localhost", 6789)],
+        host="0.0.0.0",
+        port=7404,
+    )
     await servlet.start()
-    yield
-    await servlet.stop()
-    await servlet.join()
+    await servlet.join()  # Turns out it's as simple as that.
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
