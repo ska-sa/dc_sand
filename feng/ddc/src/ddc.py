3c2432adc18ee9b3fa5258afa534b68d6c656037
avanderbyl <avanderbyl@ska.ac.za>
2020-08-27 08:06:23 +0000

    Added comments and a few extra unit tests placeholders. Next Step: A pull request to dc_sand and wait to be dissected by GC and JS :)

--- feng/ddc/src/ddc.py
+++ feng/ddc/src/ddc.py
@@ -2,20 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class DDC:
-    def __init__(self, decimation_factor: int, center_freq: float, data: np.ndarray, filter_coeffs: np.ndarray) -> None:
+    def __init__(self, decimation_factor: int, center_freq: float, data: np.ndarray, num_samples: int, filter_coeffs: np.ndarray) -> None:
         self.decimation_factor = decimation_factor
         self.center_freq = center_freq
+        self.num_samples = num_samples
         self.filter_coeffs = filter_coeffs
         self.data = data
 
-    def generate_cwg(self, freq: float, dither: bool):
+    def generate_real_cw(self, freq: float, dither: bool):
+        # Method to generate a real-valued CW vector
         pass
 
+    def generate_complx_cw(self, freq: float, dither: bool):
+        # Method to generate a complex-valued CW vector
+        pass
+    
     def decode_8bit_to_10bit_to_float_data(self, data_8bit: np.ndarray):
+        # Method to convert 8bit packed data to 10bit (initial raw) to float
         pass
 
     def decimate(self, data: np.ndarray, decimation_factor: int):
+        # Method to decimate input data by decimation factor
+        pass
+
+    def mixer(self, mixing_cw: np.ndarray, input_data: np.ndarray):
+        # Method to multiply mixing CW with input data
+        pass
+
+    def bandpass_filter(self, input_data: np.ndarray, filter_coeffs: np.ndarray):
+        # Method to apply band-pass filter to remove out-of-band products
         pass
 
     def run_ddc(self, osc: np.ndarray, data: np.ndarray, filter_coeffs: np.ndarray, decimation_factor: int):
+        # Method to implement DDC
         pass
--- feng/ddc/unit_test/test_ddc.py
+++ feng/ddc/unit_test/test_ddc.py
@@ -6,18 +6,39 @@ import numpy as np
 def DDC_test():
     decimation_factor = 1
     center_freq = 100e6
-    filter_coeffs = [0,0,0,0,0,0,0,0]
-    data = [0,0,0,0,0,0,0,0]
-    return ddc.DDC(decimation_factor=decimation_factor, center_freq=center_freq, filter_coeffs=filter_coeffs, data=data)
+    num_samples = 4096
+    filter_coeffs = []
+    data = []
+    return ddc.DDC(decimation_factor=decimation_factor, center_freq=center_freq, num_samples=num_samples, filter_coeffs=filter_coeffs, data=data)
 
-def test_cwg_gen(DDC_test):
+def test_cw_gen(DDC_test):
+    # Test to verify CW generated for mixing CW as well as test vectors
     pass
 
 def test_decimate(DDC_test):
+    # Test to verify decimation. 
     pass
 
 def test_decode_8bit_to_10bit_to_float_data(DDC_test):
+    # Test to verify conversion from packed 8bit ingest data to 10b (digitiser) data to float
     pass
 
-def test_run_ddc(DDC_test):
+def test_run_ddc_center_cw(DDC_test):
+    # Test to verify correct translation of center frequency CW down to baseband (DC) 
+    pass
+
+def test_run_ddc_dual_cw(DDC_test):
+    # Test to verify correct translation of center frequecny CW and additional in-band CW
+    pass
+
+def test_run_ddc_bandedge_cw(DDC_test):
+    # Test to verify correct translation of two in-band CW tones at band edges
+    pass
+
+def test_mixer(DDC_test):
+    # Test to verify mixing products
+    pass
+
+def test_fir_filter():
+    # Test to verify filter used to remove out-of-band components
     pass
